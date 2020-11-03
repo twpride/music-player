66cd513037ac7296d508b4594652abe7d5a3402c
howard <twpride@gmail.com>
2020-11-02 17:30:39 -0800

    static on debug true

--- fastcasual/settings.py
+++ fastcasual/settings.py
@@ -42,13 +42,13 @@ INSTALLED_APPS = [
   'django.contrib.contenttypes',
   'django.contrib.sessions',
   # 'django.contrib.messages',
-  # 'django.contrib.staticfiles',
+  'django.contrib.staticfiles',
   'debug_toolbar'
 ]
 
 MIDDLEWARE = [
   # 'django.middleware.security.SecurityMiddleware',
-  'whitenoise.middleware.WhiteNoiseMiddleware',
+  # 'whitenoise.middleware.WhiteNoiseMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   # 'django.middleware.common.CommonMiddleware',
   'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
