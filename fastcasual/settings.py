a25ab63a7efcb24280aa5db7bd9a7516e33fcbd5
howard <twpride@gmail.com>
2020-11-02 17:38:14 -0800

    no static app

--- fastcasual/settings.py
+++ fastcasual/settings.py
@@ -42,13 +42,13 @@ INSTALLED_APPS = [
   'django.contrib.contenttypes',
   'django.contrib.sessions',
   # 'django.contrib.messages',
-  'django.contrib.staticfiles',
+  # 'django.contrib.staticfiles',
   'debug_toolbar'
 ]
 
 MIDDLEWARE = [
   # 'django.middleware.security.SecurityMiddleware',
-  # 'whitenoise.middleware.WhiteNoiseMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   # 'django.middleware.common.CommonMiddleware',
   'django.middleware.csrf.CsrfViewMiddleware',
