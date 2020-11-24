182ec55b16c414ea367ea407879090b5b1149b89
howard <twpride@gmail.com>
2020-11-23 18:17:52 -0800

    delete typoi

--- webpack.config.js
+++ webpack.config.js
@@ -42,16 +42,3 @@ module.exports = {
   devtool: 'source-map'
 };
 
-// Inside of webpack.config.js:
-const WorkboxPlugin = require('workbox-webpack-plugin');
-
-module.exports = {
-  // Other webpack config...
-
-  plugins: [
-    // Other plugins...
-
-    new WorkboxPlugin.GenerateSW()
-  ]
-};
-
