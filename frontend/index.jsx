a2e3b0e8672cd8b59f9c454d9c3041aee4a1d26c
howard <twpride@gmail.com>
2020-11-23 18:15:39 -0800

    add workbox

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -43,5 +43,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
-
-registerServiceWorker()
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './frontend/index.jsx',
+  plugins: [
+    new WorkboxPlugin.GenerateSW()
+  ],
   output: {
     path: path.resolve(__dirname, 'static', 'bundles'),
     filename: "bundle.js",
@@ -38,4 +42,16 @@ module.exports = {
   devtool: 'source-map'
 };
 
+// Inside of webpack.config.js:
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+  // Other webpack config...
+
+  plugins: [
+    // Other plugins...
+
+    new WorkboxPlugin.GenerateSW()
+  ]
+};
 
