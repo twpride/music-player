e68f9b3a220c93dac90b7eb5ab3a953f98ba0d16
howard <twpride@gmail.com>
2020-11-30 12:44:43 -0800

    add dummy dup

--- song_player/views.py
+++ song_player/views.py
@@ -111,6 +111,7 @@ def song(request, id):  #get , delete
     return JsonResponse(url, safe=False)
   breakpoint()
   req = json.loads(request.body.decode('utf-8'))
+  req = json.loads(request.body.decode('utf-8'))
   song_to_delete = Song.objects.get(pk=id)
   affected_entries = song_to_delete.entry_set.all()
 
