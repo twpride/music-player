250acfe5b7cf48e3f5ac17bdbfb50cd04ef9fef6
howard <twpride@gmail.com>
2020-12-02 00:43:21 -0800

    capitalize o

--- frontend/components/audio_player.jsx
+++ frontend/components/audio_player.jsx
@@ -369,7 +369,7 @@ export default function AudioPlayer() {
     </PlayerDiv>
     <audio
       // controls
-      crossorigin="use-credentials"
+      crossOrigin="use-credentials"
       autoPlay src={songUrl}
       onEnded={skip(1)}
       onTimeUpdate={handleTimeUpdate}
