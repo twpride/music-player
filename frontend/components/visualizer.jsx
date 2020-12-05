0cb0f5fccb5d371486e8524a14143c3f6018d4b7
howard <twpride@gmail.com>
2020-12-04 23:35:19 -0800

    working log scale

--- frontend/components/visualizer.jsx
+++ frontend/components/visualizer.jsx
@@ -1,29 +1,53 @@
 
 
-import React, { useEffect,useRef, createRef, useState } from 'react';
+import React, { useEffect, useRef, createRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components'
 
 
 const VisualizerDiv = styled.div`
-  width:800px;
+flex-grow:1;
+canvas {
+  width:100%;
+  height:100%;
+}
 `
 
 export default function Visualizer({ audSource }) {
-  
-  const [canvSize, setCanvSize] = useState([800,400])
   const songD = useSelector(state => state.entities.songD);
   const playlistD = useSelector(state => state.entities.playlistD);
   const track = useSelector(state => state.player.track);
 
   audSource[0].fftSize = 8192;
-  const bufferLength = audSource[0].frequencyBinCount 
-  // const maxFreq = audSource[0].context.sampleRate /2;
-  // const cellFreqPitch = maxFreq/(bufferLength-1)
+  const bufferLength = audSource[0].frequencyBinCount
+  const totalFreqRange = audSource[0].context.sampleRate / 2;
+  const cellFreqPitch = totalFreqRange / (bufferLength - 1)
+
+
+  const minFreq = 62.5;
+  const maxFreq = 4000;
+  const minIdx = Math.floor(minFreq / cellFreqPitch)
+  const maxIdx = Math.floor(maxFreq / cellFreqPitch)
+  console.log(minIdx,maxIdx,"minmax")
+  // const logFreqRange = Math.log(maxFreq-minFreq)
+  const logMinFreq = Math.log(minFreq);
+  const logMaxFreq = Math.log(maxFreq);
+
+  const logFreqRange = logMaxFreq-logMinFreq;
+
+  const [plot_w, plot_h] = [1000, 600]
+
+  let cellXCoord = new Uint16Array(maxIdx - minIdx + 1)
+  for (let i = 0; i < cellXCoord.length; i++) {
+    const logFreqCoord= Math.log(minFreq+i*cellFreqPitch)-logMinFreq
+    cellXCoord[i] = logFreqCoord/logFreqRange*plot_w
+  }
+
+  console.log(cellFreqPitch,'cellfreqpitch')
+
 
   let dataArray = new Uint8Array(bufferLength)
   audSource[0].getByteTimeDomainData(dataArray)
-
 
   console.log(dataArray.length, 'lgn')
   console.log(audSource[0].context.sampleRate, 'srrr')
@@ -47,46 +71,45 @@ export default function Visualizer({ audSource }) {
 
 
   useEffect(() => {
+
+    console.log(dataArray.length, 'hereee')
+
+
+
     startRenderer()
-  },[])
+  }, [])
 
   function startRenderer() {
     let canvas = canvasRef.current;
     requestAnimationFrame(startRenderer);
     audSource[0].getByteFrequencyData(dataArray)
-  
+
     const ctx = canvas.getContext("2d")
     const h = canvas.height
     const w = canvas.width
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.fillRect(0, 0, w, h);
-    var barWidth = 1;
     var barHeight;
-    var x = 0;
 
-      // ctx.fillStyle = 'rgb(255,255,255)';
-      // ctx.fillRect(x,h-barHeight/2,barWidth,barHeight);
-      ctx.strokeStyle = "#FF0000";
+    ctx.strokeStyle = "white";
+
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(800, 400);
-    ctx.stroke(); 
-
-    for(var i = 0; i < bufferLength; i++) {
-    // sixteenloc = Math.floor(16000/cellFreqPitch)
-    // for(var i = sixteenloc-100; i < sixteenloc+100; i++) {
-      barHeight = dataArray[i]/2*5;
-      ctx.fillStyle = 'rgb(255,255,255)';
-      ctx.fillRect(x,h-barHeight/2,barWidth,barHeight);
-      x += barWidth + 1;
+    for (var i = minIdx; i <= maxIdx; i++) {
+      const x = cellXCoord[i-minIdx] 
+      barHeight = dataArray[i] / 2;
+      ctx.moveTo(x, h);
+      ctx.lineTo(x, h - barHeight);
     }
+    ctx.stroke()
+
   }
 
   return <VisualizerDiv>
     <canvas
-      width = {canvSize[0]}
-      height = {canvSize[1]}
       ref={canvasRef}
+      width={plot_w}
+      height={plot_h}
+      id='canvas'
     />
     {/* <img src={albumArt} /> */}
   </VisualizerDiv>
