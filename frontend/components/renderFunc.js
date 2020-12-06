98b96e465a058e96b94c5e53337a3751f7050efb
howard <twpride@gmail.com>
2020-12-06 15:00:18 -0800

    one last push

--- frontend/components/renderFunc.js
+++ frontend/components/renderFunc.js
@@ -1,5 +1,6 @@
 import { throttle } from '../util/throttle'
 export default function renderFunc(start) {
+  console.log("hereeeeee")
   this.start= start;
   this.minFreq = 200;
   const maxFreq = 2000;
@@ -7,7 +8,8 @@ export default function renderFunc(start) {
   const minDecibels = -60;
   const maxDecibels = 0;
 
-  // const aud = document.getElementById('audio')
+  const aud = document.getElementById('audio')
+  // aud.disconnect()
   this.container = document.getElementById('container')
   this.canvas = document.createElement('canvas')
   this.canvas.width = this.container.clientWidth;
@@ -15,58 +17,50 @@ export default function renderFunc(start) {
   this.ctx = this.canvas.getContext("2d")
   this.container.appendChild(this.canvas)
 
-  // const audctx = new (window.AudioContext || window.webkitAudioContext)()
-  // const source = audctx.createMediaElementSource(aud)
-  // const splitter = audctx.createChannelSplitter(2);
-  // this.analyzer = [];
-  // for (let i = 0; i < 2; i++) {
-  //   const _analyzer = audctx.createAnalyser();
-  //   _analyzer.fftSize = fftSize;
-  //   _analyzer.minDecibels = minDecibels;
-  //   _analyzer.maxDecibels = maxDecibels;
-  //   splitter.connect(_analyzer, i);
-  //   this.analyzer.push(_analyzer)
-  // }
-  // const delay = audctx.createDelay(0.26)
-  // source.connect(splitter)
-  // source.connect(delay)
-  // delay.connect(audctx.destination)
-
-  // const bufferLength = this.analyzer[0].frequencyBinCount
-  // const totalFreqRange = this.analyzer[0].context.sampleRate / 2;
-  // this.cellFreqPitch = totalFreqRange / (bufferLength - 1)
-  // this.minIdx = Math.floor(this.minFreq / this.cellFreqPitch)
-  // this.maxIdx = Math.floor(maxFreq / this.cellFreqPitch)
-  // this.logMinFreq = Math.log(this.minFreq);
-  // this.logMaxFreq = Math.log(maxFreq);
-  // this.logFreqRange = this.logMaxFreq - this.logMinFreq;
-
-  // this.cellXCoord = new Int16Array(this.maxIdx - this.minIdx + 1)
-  // for (let i = 0; i < this.cellXCoord.length; i++) {
-  //   const logFreqCoord = Math.log(this.minFreq + i * this.cellFreqPitch) - this.logMinFreq
-  //   this.cellXCoord[i] = Math.floor(logFreqCoord / this.logFreqRange * this.canvas.width)
-  // }
-
-  // console.log(this.cellXCoord.length, "cost")
-  // this.dataArray = new Uint8Array(bufferLength)
-  // // ctx.strokeStyle = "#ad0f37";
-  // this.ctx.strokeStyle = "lightgrey";
-  // this.ctx.fillStyle = "lightgrey";
-  // // ctx.beginPath();
-  // // ctx.moveTo(0,0);
-  // // ctx.lineTo(canvas.width, canvas.height);
-  // // ctx.stroke()
-
-
-  const aud = document.getElementById('audio')
-  aud.disconnect()
-  // var AudioContext = window.webkitAudioContext;
-  var AudioContext = window.AudioContext
-  // var AudioContext = window.AudioContext || window.webkitAudioContext;
-  var audctx = new AudioContext();
-  // const audctx = new (window.AudioContext || window.webkitAudioContext)()
+  const audctx = new (window.AudioContext || window.webkitAudioContext)()
   const source = audctx.createMediaElementSource(aud)
-  source.connect(audctx.destination)
+  const splitter = audctx.createChannelSplitter(2);
+  this.analyzer = [];
+  for (let i = 0; i < 2; i++) {
+    const _analyzer = audctx.createAnalyser();
+    _analyzer.fftSize = fftSize;
+    _analyzer.minDecibels = minDecibels;
+    _analyzer.maxDecibels = maxDecibels;
+    splitter.connect(_analyzer, i);
+    this.analyzer.push(_analyzer)
+  }
+  const delay = audctx.createDelay(0.26)
+  source.connect(splitter)
+  source.connect(delay)
+  delay.connect(audctx.destination)
+
+  const bufferLength = this.analyzer[0].frequencyBinCount
+  const totalFreqRange = this.analyzer[0].context.sampleRate / 2;
+  this.cellFreqPitch = totalFreqRange / (bufferLength - 1)
+  this.minIdx = Math.floor(this.minFreq / this.cellFreqPitch)
+  this.maxIdx = Math.floor(maxFreq / this.cellFreqPitch)
+  this.logMinFreq = Math.log(this.minFreq);
+  this.logMaxFreq = Math.log(maxFreq);
+  this.logFreqRange = this.logMaxFreq - this.logMinFreq;
+
+  this.cellXCoord = new Int16Array(this.maxIdx - this.minIdx + 1)
+  for (let i = 0; i < this.cellXCoord.length; i++) {
+    const logFreqCoord = Math.log(this.minFreq + i * this.cellFreqPitch) - this.logMinFreq
+    this.cellXCoord[i] = Math.floor(logFreqCoord / this.logFreqRange * this.canvas.width)
+  }
+
+  console.log(this.cellXCoord.length, "cost")
+  this.dataArray = new Uint8Array(bufferLength)
+  // ctx.strokeStyle = "#ad0f37";
+  this.ctx.strokeStyle = "lightgrey";
+  this.ctx.fillStyle = "lightgrey";
+  // ctx.beginPath();
+  // ctx.moveTo(0,0);
+  // ctx.lineTo(canvas.width, canvas.height);
+  // ctx.stroke()
+
+
+
 
   window.addEventListener('resize', throttle(() => {
     console.log(this)
@@ -77,7 +71,9 @@ export default function renderFunc(start) {
   }, 200).bind(this))
 
   window.addEventListener("beforeunload", function(event) { 
-
+    this.source.disconnect()
+    this.source = null;
+    console.log('hereeeeeexxx')
    });
 
 }
@@ -91,28 +87,28 @@ renderFunc.prototype.setCanvas = function () {
 
 renderFunc.prototype.startRenderer = function () {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  // this.ctx.beginPath();
-  // this.ctx.moveTo(0, this.canvas.height / 2);
+  this.ctx.beginPath();
+  this.ctx.moveTo(0, this.canvas.height / 2);
 
-  // this.analyzer[0].getByteFrequencyData(this.dataArray)
-  // for (var i = this.minIdx; i <= this.maxIdx; i++) {
-  //   const x = this.cellXCoord[i - this.minIdx]
-  //   this.ctx.lineTo(x, this.canvas.height / 2 - this.dataArray[i] / 8);
-  // }
+  this.analyzer[0].getByteFrequencyData(this.dataArray)
+  for (var i = this.minIdx; i <= this.maxIdx; i++) {
+    const x = this.cellXCoord[i - this.minIdx]
+    this.ctx.lineTo(x, this.canvas.height / 2 - this.dataArray[i] / 8);
+  }
 
-  // this.analyzer[1].getByteFrequencyData(this.dataArray)
-  // for (var i = this.maxIdx; i >= this.minIdx; i--) {
-  //   const x = this.cellXCoord[i - this.minIdx]
-  //   this.ctx.lineTo(x, this.canvas.height / 2 + this.dataArray[i] / 8 );
-  // }
+  this.analyzer[1].getByteFrequencyData(this.dataArray)
+  for (var i = this.maxIdx; i >= this.minIdx; i--) {
+    const x = this.cellXCoord[i - this.minIdx]
+    this.ctx.lineTo(x, this.canvas.height / 2 + this.dataArray[i] / 8 );
+  }
 
-  // this.ctx.closePath()
-  // this.ctx.fill()
-  // this.ctx.stroke()
-  console.log('heree')
-  this.ctx.font = "30px Arial";
-  // this.ctx.fillText(Date.now()-this.start,100,50)
-  this.ctx.fillText(window.AudioContext,100,20)
-  this.ctx.fillText(window.webkitAudioContext,100,40)
+  this.ctx.closePath()
+  this.ctx.fill()
+  this.ctx.stroke()
+  // console.log('heree')
+  // this.ctx.font = "30px Arial";
+  // // this.ctx.fillText(Date.now()-this.start,100,50)
+  // this.ctx.fillText(window.AudioContext,100,20)
+  // this.ctx.fillText(window.webkitAudioContext,100,40)
   requestAnimationFrame(this.startRenderer.bind(this));
 }
