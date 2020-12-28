c302d9b3eba64562af12df237ec3a8ca0ef6318f
howard <twpride@gmail.com>
2020-12-28 13:33:26 -0800

    with viz enabled

--- frontend/components/audio_player.jsx
+++ frontend/components/audio_player.jsx
@@ -350,7 +350,7 @@ export default function AudioPlayer({ winWidth }) {
       // controls
       id='audio'
       ref={aud}
-      // crossOrigin="anonymous"
+      crossOrigin="anonymous"
       autoPlay
       src={songUrl}
       onEnded={skip(1)}
--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import styled from 'styled-components'
 
-// import AudioVisualizer from './audio_visualizer'
+import AudioVisualizer from './audio_visualizer'
 import { useLocation } from 'react-router-dom'
 import { context_act } from '../reducers/ui_reducer';
 import { HoverAccount, HoverGithub, HoverLinkedin, DarkIcon, LightIcon } from './active_svgs'
@@ -62,32 +62,32 @@ export default function Header({ darkMode, setDarkMode }) {
   const [title, setTitle] = useState('')
   const containerRef = useRef()
   const location = useLocation()
-  // useEffect(() => {
-  //   function resumeAudioCtx(e) {
-  //     visualizer.paused = false;
-  //     visualizer.startRenderer()
-  //   }
-  //   function mobileVizWorkaround(e) {
-  //     visualizer.audctx.resume()
-  //     window.removeEventListener('touchend', mobileVizWorkaround)
-  //   }
+  useEffect(() => {
+    function resumeAudioCtx(e) {
+      visualizer.paused = false;
+      visualizer.startRenderer()
+    }
+    function mobileVizWorkaround(e) {
+      visualizer.audctx.resume()
+      window.removeEventListener('touchend', mobileVizWorkaround)
+    }
 
-  //   function suspendAudioCtx(e) {
-  //     visualizer.paused = true;
-  //   }
-  //   const visualizer = new AudioVisualizer(containerRef.current)
-  //   window.viz = visualizer;
-  //   visualizer.startRenderer()
+    function suspendAudioCtx(e) {
+      visualizer.paused = true;
+    }
+    const visualizer = new AudioVisualizer(containerRef.current)
+    window.viz = visualizer;
+    visualizer.startRenderer()
 
-  //   window.addEventListener('touchend', mobileVizWorkaround)
-  //   window.addEventListener('resumeViz', resumeAudioCtx)
-  //   window.addEventListener('suspendViz', suspendAudioCtx)
+    window.addEventListener('touchend', mobileVizWorkaround)
+    window.addEventListener('resumeViz', resumeAudioCtx)
+    window.addEventListener('suspendViz', suspendAudioCtx)
 
-  //   return () => {
-  //     window.removeEventListener('resumeViz', resumeAudioCtx)
-  //     window.removeEventListener('suspendViz', suspendAudioCtx)
-  //   }
-  // }, [])
+    return () => {
+      window.removeEventListener('resumeViz', resumeAudioCtx)
+      window.removeEventListener('suspendViz', suspendAudioCtx)
+    }
+  }, [])
 
   useEffect(() => {
     const locArr = location.pathname.split('/')
